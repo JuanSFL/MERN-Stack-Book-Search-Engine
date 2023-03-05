735795135e92fb34bbe1192494b9f8b85444c35c
JuanSFL <molina98j@yahoo.com>
2023-03-04 23:22:24 -0500

    added code to savedBooks.js to execute the GET_ME query and REMOVE_BOOK mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Col,
-  Form,
-  Button,
-  Card,
-  Row
-} from 'react-bootstrap';
-
+import {Container, Col, Form,Button,Card, Row} from 'react-bootstrap';
+ 
 import Auth from '../utils/auth';
-//will replace these
+
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations.js';
 
