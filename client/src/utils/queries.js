004da5643fc374127e33be082bb0b2a40cd8af37
JuanSFL <molina98j@yahoo.com>
2023-03-04 22:09:47 -0500

    removed unnesecary code from app.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,21 +1,19 @@
-import gql from 'graphql-tag';
-
+import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-{
+  query me {
     me {
       _id
       username
       email
       bookCount
       savedBooks {
-        # _id
         bookId
         authors
+        description
         image
         link
         title
-        description
       }
     }
   }
